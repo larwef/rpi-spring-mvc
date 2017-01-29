88bdca17b74fd0e048273e4223eb40b7ed69c88e
Lars Wefald <larwef@gmail.com>
2017-01-29 21:18:22 +0100

    Convenient checkpoint

---
+++ src/main/java/rpiSpringMvc/Config/AppConfig.java
@@ -0,0 +1,10 @@
+package rpiSpringMvc.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="rpiSpringMvc")
+public class AppConfig {
+
+}
--- src/main/java/rpiSpringMvc/Main.java
+++ src/main/java/rpiSpringMvc/Main.java
@@ -1,7 +1,26 @@
 package rpiSpringMvc;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import org.springframework.web.servlet.DispatcherServlet;
+import rpiSpringMvc.Config.AppConfig;
+import rpiSpringMvc.Config.ApplicationConfig;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(ApplicationConfig.SERVICE_PORT);
+        ServletContextHandler context = new ServletContextHandler(server, ApplicationConfig.SERVICE_CONTEXT);
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        dispatcherServlet.setContextConfigLocation("rpiSpringMvc.Config");
+
+        ServletHolder servletHolder = new ServletHolder(dispatcherServlet);
+        context.addServlet(servletHolder, "/*");
+
+        server.start();
+        server.join();
     }
 }
