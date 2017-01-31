317109812895ae9847e55e435c4f99dfe6f2d257
Lars Wefald <larwef@gmail.com>
2017-01-31 21:41:47 +0100

    Added shade plugin to gradle. Replaced script with makefile

--- docker/test-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printf "\nBuilding docker image..."
-docker build -t rpi-spring-mvc .
-printf "\nRunning docker image...\n"
-docker run -it --rm -p 8087:8087 rpi-spring-mvc
--- docker/transfer-to-pi.sh
+++ docker/transfer-to-pi.sh
@@ -3,6 +3,6 @@
 PI_IP="192.168.1.31"
 PI_USER="pi"
 
-cp -p "$(ls ../build/libs/*SNAPSHOT.jar | grep -v /orig | head -1)" toRoot/app.jar
+cp -p "$(ls ../build/libs/*SNAPSHOT*.jar | grep -v /orig | head -1)" toRoot/app.jar
 
 rsync -r ../docker/ . $PI_USER@$PI_IP:rpi-spring-mvc/
