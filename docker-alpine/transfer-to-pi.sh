b1ec675a582292b85798db33ad550446d746f36c
Lars Wefald <larwef@gmail.com>
2017-05-13 16:57:53 +0200

    Small update to build script

--- docker-alpine/transfer-to-pi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PI_IP="192.168.1.31"
-PI_USER="pi"
-
-cp -p "$(ls ../build/libs/*SNAPSHOT*.jar | grep -v /orig | head -1)" toRoot/app.jar
-
-rsync -r ../docker/ . $PI_USER@$PI_IP:rpi-spring-mvc/
